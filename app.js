a10c65fee92ea9a9f2195cca3eb318d045980f7e
Muhammed Ensar Canbay <mecanbay@pm.me>
2022-06-13 16:55:48 +0300

    Çalışma alanı oluşturuldu.

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 
+// ROUTES
+const pageRouter = require("./routes/pageRouter");
 
+// TEMPLATE ENGINE
+app.set("view engine", "ejs");
 
+//DATABASE CONNECTION
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+  console.log("MongoDB Bağlantısı Başarıyla Gerçekleşti.");
+});
+
+// MIDDLEWARES
+app.use(express.static("public"));
+
+app.get("/", pageRouter);
 
 app.listen(process.env.APP_PORT, () => {
-    console.log(`Sunucu ${process.env.APP_PORT} portunda ayağa kalktı.`);
-})
+  console.log(`Sunucu ${process.env.APP_PORT} portunda ayağa kalktı.`);
+});
